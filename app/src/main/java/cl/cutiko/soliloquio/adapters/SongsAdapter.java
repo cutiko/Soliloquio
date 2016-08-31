2e7903f2df56ec4c20f230261c781476abb242f6
cutiko <cutiko@gmail.com>
2016-08-31 02:04:59 -0300

    ADD: basic adapter working

---
+++ app/src/main/java/cl/cutiko/soliloquio/adapters/SongsAdapter.java
@@ -0,0 +1,53 @@
+package cl.cutiko.soliloquio.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cl.cutiko.soliloquio.R;
+
+/**
+ * Created by cutiko on 31-08-16.
+ */
+public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.ViewHolder> {
+
+    private List<String> songs;
+
+    public SongsAdapter(List<String> songs) {
+        this.songs = songs;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_song, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Log.d("ADAPTER", String.valueOf(position));
+        holder.name.setText(songs.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+
+        ViewHolder(View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.songName);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/cl/cutiko/soliloquio/models/Song.java
@@ -0,0 +1,21 @@
+package cl.cutiko.soliloquio.models;
+
+/**
+ * Created by cutiko on 31-08-16.
+ */
+public class Song {
+
+    private String name;
+
+    public Song(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
