cc29062807b14ccebfa342cf906b5c86c43187b4
cutiko <cutiko@gmail.com>
2016-09-01 00:34:07 -0300

    ADD: title is gone while expanded

--- app/src/main/java/cl/cutiko/soliloquio/views/main/MainActivity.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/main/MainActivity.java
@@ -1,9 +1,11 @@
 package cl.cutiko.soliloquio.views.main;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -31,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+        collapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, android.R.color.transparent));
+
     }
 
     @Override
