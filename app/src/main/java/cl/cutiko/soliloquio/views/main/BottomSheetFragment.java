2b721bed6ec2ec290a13bf8812f30b9535aafdc8
cutiko <cutiko@gmail.com>
2016-09-03 00:39:23 -0300

    FIX: bottom sheet is not hidable

--- app/src/main/java/cl/cutiko/soliloquio/views/main/BottomSheetFragment.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/main/BottomSheetFragment.java
@@ -2,11 +2,14 @@ package cl.cutiko.soliloquio.views.main;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import cl.cutiko.soliloquio.R;
 import info.abdolahi.CircularMusicProgressBar;
