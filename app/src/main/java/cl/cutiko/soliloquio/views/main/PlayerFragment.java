4343fb133d48bb29efe4013ee3626f6cf2b35dd3
cutiko <cutiko@gmail.com>
2016-09-19 13:21:58 -0300

    REMOVE: todo and import

--- app/src/main/java/cl/cutiko/soliloquio/views/main/PlayerFragment.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/main/PlayerFragment.java
@@ -6,13 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import cl.cutiko.soliloquio.R;
 import cl.cutiko.soliloquio.adapters.SongsAdapter;
@@ -26,7 +22,6 @@ public class PlayerFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO BUNDLE THE SERVICE PLAYER
     }
 
     @Override
