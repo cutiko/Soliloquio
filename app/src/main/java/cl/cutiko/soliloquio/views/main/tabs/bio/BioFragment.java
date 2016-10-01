0f4269432eac3bb2fc17950ea63ce43cf554e2ce
cutiko <cutiko@gmail.com>
2016-10-01 17:55:12 -0300

    ADD: social share

--- app/src/main/java/cl/cutiko/soliloquio/views/main/tabs/bio/BioFragment.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/main/tabs/bio/BioFragment.java
@@ -1,28 +1,12 @@
 package cl.cutiko.soliloquio.views.main.tabs.bio;
 
 
-import android.content.Intent;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.share.Sharer;
-import com.facebook.share.model.ShareLinkContent;
-import com.facebook.share.widget.ShareDialog;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.List;
 
 import cl.cutiko.soliloquio.R;
 
@@ -42,60 +26,5 @@ public class BioFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // Create intent using ACTION_VIEW and a normal Twitter url:
-        String tweetUrl = String.format("https://twitter.com/intent/tweet?text=%s&url=%s",
-                urlEncode("Tweet text"),
-                urlEncode("https://www.google.fi/"));
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(tweetUrl));
-
-        // Narrow down to official Twitter app, if available:
-        List<ResolveInfo> matches = getActivity().getPackageManager().queryIntentActivities(intent, 0);
-        for (ResolveInfo info : matches) {
-            if (info.activityInfo.packageName.toLowerCase().startsWith("com.twitter")) {
-                intent.setPackage(info.activityInfo.packageName);
-            }
-        }
-
-        /*startActivity(intent);*/
-
-        ShareLinkContent content = new ShareLinkContent.Builder()
-                .setContentUrl(Uri.parse("https://developers.facebook.com"))
-                .build();
-
-        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
-        CallbackManager callbackManager = new CallbackManager.Factory().create();
-        ShareDialog shareDialog = new ShareDialog(getActivity());
-        shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
-            @Override
-            public void onSuccess(Sharer.Result result) {
-
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
-            }
-        });
-        if (ShareDialog.canShow(ShareLinkContent.class)) {
-            /*shareDialog.show(content);*/
-        }
-        /*https://developers.facebook.com/docs/sharing/android*/
-        /*http://android-arsenal.com/details/1/2740*/
-    }
-
-    public static String urlEncode(String s) {
-        try {
-            return URLEncoder.encode(s, "UTF-8");
-        }
-        catch (UnsupportedEncodingException e) {
-            Log.wtf("URL_ENCODE", "UTF-8 should always be supported", e);
-            throw new RuntimeException("URLEncoder.encode() failed for " + s);
-        }
     }
 }
--- app/src/main/java/cl/cutiko/soliloquio/views/main/tabs/bio/ShareFragment.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/main/tabs/bio/ShareFragment.java
@@ -1,11 +1,30 @@
 package cl.cutiko.soliloquio.views.main.tabs.bio;
 
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import cl.cutiko.soliloquio.R;
 
@@ -14,6 +33,10 @@ import cl.cutiko.soliloquio.R;
  */
 public class ShareFragment extends Fragment {
 
+    private ImageButton facebookBtn, twitterBtn, shareBtn;
+    private CallbackManager callbackManager;
+    private ShareDialog shareDialog;
+    private Intent twitterIntent;
 
     public ShareFragment() {
         // Required empty public constructor
@@ -27,4 +50,115 @@ public class ShareFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_share, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setFacebook();
+        setTwitter();
+
+        facebookBtn = (ImageButton) view.findViewById(R.id.facebookBtn);
+        twitterBtn = (ImageButton) view.findViewById(R.id.twitterBtn);
+        shareBtn = (ImageButton) view.findViewById(R.id.shareBtn);
+
+        facebookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                facebookShare();
+            }
+        });
+
+        twitterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                twitterShare();
+            }
+        });
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                share();
+            }
+        });
+    }
+
+    private void setTwitter() {
+        // Create intent using ACTION_VIEW and a normal Twitter url:
+        String tweetUrl = String.format("https://twitter.com/intent/tweet?text=%s&url=%s",
+                urlEncode(getString(R.string.share_msg)),
+                urlEncode(getString(R.string.share_url)));
+        twitterIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(tweetUrl));
+
+        // Narrow down to official Twitter app, if available:
+        List<ResolveInfo> matches = getActivity().getPackageManager().queryIntentActivities(twitterIntent, 0);
+        for (ResolveInfo info : matches) {
+            if (info.activityInfo.packageName.toLowerCase().startsWith("com.twitter")) {
+                twitterIntent.setPackage(info.activityInfo.packageName);
+            }
+        }
+
+    }
+
+    private void setFacebook() {
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        callbackManager = new CallbackManager.Factory().create();
+        shareDialog = new ShareDialog(getActivity());
+        shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+            @Override
+            public void onSuccess(Sharer.Result result) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    public static String urlEncode(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            Log.wtf("URL_ENCODE", "UTF-8 should always be supported", e);
+            throw new RuntimeException("URLEncoder.encode() failed for " + s);
+        }
+    }
+
+    private void facebookShare(){
+        ShareLinkContent content = new ShareLinkContent.Builder()
+                .setContentUrl(Uri.parse(getString(R.string.share_url)))
+                .build();
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            shareDialog.show(content);
+        }
+    }
+
+    private void twitterShare(){
+        startActivity(twitterIntent);
+    }
+
+    private void share(){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_msg) + " " + getString(R.string.share_url));
+        sendIntent.setType("text/plain");
+
+
+        if (sendIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(sendIntent);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
