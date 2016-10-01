0938d3dd2d523669b7a46c9f8a6aa43130bda44e
cutiko <cutiko@gmail.com>
2016-10-01 10:51:10 -0300

    ADD: jingle for splash

--- app/src/main/java/cl/cutiko/soliloquio/views/splash/SplashActivity.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/splash/SplashActivity.java
@@ -1,9 +1,13 @@
 package cl.cutiko.soliloquio.views.splash;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
 
 import cl.cutiko.soliloquio.R;
 import cl.cutiko.soliloquio.views.main.MainActivity;
@@ -20,14 +24,32 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
-        particleView = (ParticleView) findViewById(R.id.introParticle);
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+        Uri uriSong = Uri.parse("android.resource://cl.cutiko.soliloquio/raw/" + R.raw.principios);
+        try {
+            mediaPlayer.setDataSource(this, uriSong);
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        particleView.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onAnimationEnd() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            public void run() {
+               if (mediaPlayer.isPlaying()) {
+                   mediaPlayer.stop();
+               }
+               startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
-        });
+        }, 5000);
+
+        particleView = (ParticleView) findViewById(R.id.introParticle);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -36,6 +58,8 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 200);
     }
+
+
 
 
 }
