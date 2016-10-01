fd7efa3b856a0e3fdfe4a597cda21cea85752170
cutiko <cutiko@gmail.com>
2016-10-01 11:10:02 -0300

    ADD: second tab text

--- app/src/main/java/cl/cutiko/soliloquio/views/main/MainActivity.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/main/MainActivity.java
@@ -62,6 +62,6 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
     }
 
-    //TODO jingle, el 2do tab, el share, el icono, las notificaciones
+    //TODO el share, loading para la lista, el icono, las notificaciones
 
 }
