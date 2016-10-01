17a212906aec83351a58b2b6dc04b96b0334f983
cutiko <cutiko@gmail.com>
2016-10-01 11:40:53 -0300

    WIP: social share

--- app/src/main/java/cl/cutiko/soliloquio/views/main/tabs/BioFragment.java
+++ app/src/main/java/cl/cutiko/soliloquio/views/main/tabs/BioFragment.java
@@ -1,12 +1,21 @@
 package cl.cutiko.soliloquio.views.main.tabs;
 
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
 
 import cl.cutiko.soliloquio.R;
 
@@ -27,5 +36,32 @@ public class BioFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Create intent using ACTION_VIEW and a normal Twitter url:
+        String tweetUrl = String.format("https://twitter.com/intent/tweet?text=%s&url=%s",
+                urlEncode("Tweet text"),
+                urlEncode("https://www.google.fi/"));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(tweetUrl));
+
+        // Narrow down to official Twitter app, if available:
+        List<ResolveInfo> matches = getActivity().getPackageManager().queryIntentActivities(intent, 0);
+        for (ResolveInfo info : matches) {
+            if (info.activityInfo.packageName.toLowerCase().startsWith("com.twitter")) {
+                intent.setPackage(info.activityInfo.packageName);
+            }
+        }
+
+        /*startActivity(intent);*/
+
+        /*https://developers.facebook.com/docs/sharing/android*/
+    }
+
+    public static String urlEncode(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            Log.wtf("URL_ENCODE", "UTF-8 should always be supported", e);
+            throw new RuntimeException("URLEncoder.encode() failed for " + s);
+        }
     }
 }
